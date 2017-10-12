9af71b43e025ebdb22567c02398c8f0e84caee3f
Aleksandr Loktev <al@avis.codes>
2017-10-12 13:25:19 +0300

    Second sprint by "Kino 2" project and bug fix.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function ($) {
+
   //buffer equal footer implementation
   function footerHeight() {
     var footerHeight = $(".footer").innerHeight();
@@ -6,6 +7,7 @@
     $(".page-wrap").css('margin-bottom', -footerHeight+'px');
   }
 
+  // implementation function for main slider
   function mainSlider() {
       $(".header-slider__flexslider").flexslider({
         animation: 'slide',
@@ -13,11 +15,68 @@
         useCSS: false,
         slideshow: true,
         slideshowSpeed: 7000,
-        keyboard: false
+        keyboard: false,
+        start: function(){
+          $('.slide-item').matchHeight();
+        }
       });
   }
 
+  // implementation function for top menu
+  function topMenu() {
+    var i = 1;
 
+
+    $('.menu__drop-item > a').on("click", function () {
+      if(i % 2 != 0) {
+        $(this).find('+.menu').slideDown();
+        i++;
+      }
+
+      else {
+        $(this).find('+.menu').slideUp();
+        i++;
+      }
+
+      return false;
+    });
+
+    $('body').on("click", function () {
+      if(i % 2 == 0) {
+        $('.menu__drop-item a + .menu').slideUp();
+        i++;
+      }
+    });
+  }
+
+  // implementation function for search form
+  function search() {
+     var i = 1;
+
+      $(".icon").on("click", function () {
+        if (i % 2 == 0) {$(".top-menu-wrap").removeClass('search-open');
+          $(".icon").removeClass('icon-close').addClass('icon-search');
+          i++;
+        }
+
+        else {
+            $(".top-menu-wrap").addClass('search-open');
+            $(".icon").removeClass('icon-search').addClass('icon-close');
+            i++;
+        }
+      });
+  }
+
+  // implemintation change top menu background color
+  function scrollTop($top) {
+    if($(window).scrollTop() > $top) {
+      $(".top-menu").addClass('scroll-menu_blue');
+    }
+
+    else {
+      $(".top-menu").removeClass('scroll-menu_blue');
+    }
+  }
 
   $(document).ready(function() {
     // Code to run when the document is ready.
@@ -26,19 +85,55 @@
     //buffer equal footer call
     footerHeight();
 
+    //main slider call
     mainSlider();
+
+    //top menu call
+    topMenu();
+
+    //search form call
+    search();
+
+    // mobile menu implemintation and call
+    if(window.innerWidth < 1024) {
+      $('#menu').mmenu({
+        "counters": true,
+        "extensions": [
+          "pagedim-black"
+        ],
+        "offCanvas": {
+          "position": "right",
+          "zposition": "front",
+        },
+        // 'navbar': {
+        //   'add': false
+        // }
+      });
+    }
+  });
+
+  $(window).scroll(function () {
+
+    //call change top menu background color
+    if(window.innerWidth > 1024) {
+      scrollTop(200);
+    }
+
+    if(window.innerWidth <= 1024) {
+      scrollTop(50);
+    }
+
   });
 
   $(window).load(function() {
     // executes when complete page is fully loaded, including all frames, objects and images
     console.log('Complete page is fully loaded.');
-    $('.slide-item').matchHeight();
   });
 
   $(window).resize(function () {
+
     //buffer equal footer call
     footerHeight();
-    $('.slide-item').matchHeight();
   });
 
 })(jQuery);
